bab3476f2e24d544b8944057918fc529e0b8023b
Unknown <up201705616@fe.up.pt>
2019-05-07 00:29:10 +0100

    !!!!!!!!!

--- tp05/Submission/ex5-T5G06-1/ShaderScene.js
+++ tp05/Submission/ex5-T5G06-1/ShaderScene.js
@@ -83,7 +83,7 @@ class ShaderScene extends CGFscene {
 		this.testShaders[5].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ timeFactor: 0 });
-		this.testShaders[12].setUniformsValues({ waterTex: 2, uSampler4: 3 , timeFactor: 0});
+		this.testShaders[12].setUniformsValues({ waterTex: 2, waterMap: 3 , timeFactor: 0});
 
 		// Shaders interface variables
 
@@ -219,7 +219,11 @@ class ShaderScene extends CGFscene {
 		// bind additional texture to texture unit 1
 		this.texture2.bind(1);
 		this.waterTex.bind(2);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
 		this.waterMap.bind(3);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
 
 		//Uncomment following lines in case texture must have wrapping mode 'REPEAT'
 		//this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
--- tp05/Submission/ex5-T5G06-1/shaders/water.vert
+++ tp05/Submission/ex5-T5G06-1/shaders/water.vert
@@ -10,10 +10,11 @@ uniform sampler2D waterMap;
 uniform float timeFactor;
 
 void main() {
+	float t = timeFactor;
 	vec4 color = texture2D(waterMap, aTextureCoord + vec2(timeFactor*.00114,timeFactor * 0.027));
-	float v = 0.07 * ((color.g + color.r + color.b)/3.0 - 0.5);
-	float z = 0.02 * sin( 2.0*( 0.01*timeFactor + aTextureCoord[0] + aTextureCoord[1]));
-	vec3 offset = vec3(0 , 0 , v + z);
+	float x = 0.07 * ((color.g + color.r + color.b)/3.0 - 0.5);
+	float y = 0.02 * sin( 2.0*( 0.01*t + aTextureCoord[0] + aTextureCoord[1]));
+	vec3 offset = vec3(0 , 0 , x + y);
 	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition + offset, 1.0);
 	vTextureCoord = aTextureCoord;
 }
