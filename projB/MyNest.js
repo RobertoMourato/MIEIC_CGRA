a1b820e544e8266fe9f1c3799a41b326bc5e685e
Miguel Simões <miguelsimoes20000720@gmail.com>
2019-06-04 04:05:29 +0100

    Added 4

--- projB/MyNest.js
+++ projB/MyNest.js
@@ -72,5 +72,33 @@ class MyNest extends CGFobject {
         this.scene.trunkTexture.apply();
         this.nest[7].display();
         this.scene.popMatrix();
+
+        /*this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 1, 0, 4);
+        this.scene.translate(2.5, -13.5, 8);
+        this.scene.trunkTexture.apply();
+        this.nest[8].display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 1, 1, 0);
+        this.scene.translate(4, 10, 10.5);
+        this.scene.trunkTexture.apply();
+        this.nest[9].display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 1, 1, 0);
+        this.scene.translate(4.5, 9, 10);
+        this.scene.trunkTexture.apply();
+        this.nest[10].display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.translate(14.5, 4.2, -0.3);
+        this.scene.trunkTexture.apply();
+        this.nest[11].display();
+        this.scene.popMatrix();*/
     }
 }
